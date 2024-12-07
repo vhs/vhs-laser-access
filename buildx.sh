ab1fad383b8b1f0229c64c01e715baa6af00a800
Ty Eggen <tycho.eggen@gmail.com>
2024-12-07 20:54:29 +0000

    fix(ci): make image tags safe

--- buildx.sh
+++ buildx.sh
@@ -6,6 +6,10 @@ if [ "${TEMPLATE}" = "" ] ; then
 	TEMPLATE=vanhack/vhs-laser-access:buildx-test
 fi
 
+TEMPLATE_PATH=$(echo "${TEMPLATE}" | cut -f1 -d:)
+TEMPLATE_TAG=$(echo "${TEMPLATE}" | cut -f2- -d: | sed 's:/:-:g')
+
+TEMPLATE="${TEMPLATE_PATH}:${TEMPLATE_TAG}"
 
 if [ -f .build.env ]; then
         . .build.env
