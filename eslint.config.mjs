84ad0b9839e50265edd4b727da203bae96ea0f85
Ty Eggen <tycho.eggen@gmail.com>
2024-12-08 01:09:43 +0000

    dev: eslint globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,12 @@ import js from '@tyisi/config-eslint/js'
 
 js[0].files.push('bin/www')
 
+js[0].languageOptions.globals.angular = false
+js[0].languageOptions.globals.io = false
+
+js[0].languageOptions.globals.describe = false
+js[0].languageOptions.globals.it = false
+js[0].languageOptions.globals.before = false
+js[0].languageOptions.globals.after = false
+
 export default js
