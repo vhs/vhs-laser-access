a91a4688e92f7fb556c9cc283fc9aa72b8fd75a5
Cory Alder <cory@crowselectromusic.com>
2025-10-21 15:23:33 -0700

    comment out route setup

--- src/LaserWebApp.ts
+++ src/LaserWebApp.ts
@@ -20,22 +20,28 @@ export class LaserWebApp {
     }
 
     init() {
-        this.laserController.addMiddleware(this.expressApp)
-        this.expressApp.use('/', this.laserController.router)
-
-        this.laserController.addErrorHandlers(this.expressApp)
-
+        // setup view engine & static file delivery
         this.expressApp.set('views', path.join(__dirname, '..', 'views'))
         this.expressApp.set('view engine', 'pug')
-
         this.expressApp.use(express.static(path.join(__dirname, '..', 'public')))
 
+        // setup socket.io middleware
+        this.laserController.addMiddleware(this.expressApp)
+
+        // mount the laser controller routes at '/'
+        this.expressApp.use('/', this.laserController.router)
+
+        // laser controller has special error handler for API routes
+        this.laserController.addErrorHandlers(this.expressApp)
+
+        // catch unhandled routes, create a 404 error
         this.expressApp.use(function (_req: Request, _res: Response, next: NextFunction) {
             const err = new Error('Not Found') as Error & { status?: number }
             err.status = 404
             next(err)
         })
 
+        // main error handler, renders an html error page
         this.expressApp.use((err: unknown, _req: Request, res: Response, _next: NextFunction) => {
             // narrow unknown to Error-like
             const e = err as Error & { status?: number }
